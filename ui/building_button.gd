7918f65f5af2ead71a0d5f3383ddac8a000e1d1a
Mathis Robert <mathis.robert@sib.fr>
2021-07-08 14:54:07 +0200

    Updated Building Data with textures

--- ui/building_button.gd
+++ ui/building_button.gd
@@ -4,26 +4,18 @@ extends MarginContainer
 signal selected_building
 
 
-const FILES = {
-	"Maison 1": "res://assets/sprites/buildings/house_1.png",
-	"Maison 2": "res://assets/sprites/buildings/house_2.png",
-	"Maison 3": "res://assets/sprites/buildings/house_3.png",
-	"Mairie": "res://assets/sprites/buildings/city_hall.png",
-	"Hôpital": "res://assets/sprites/buildings/hospital.png",
-	"Commissariat": "res://assets/sprites/buildings/police_department.png"
-}
 
 
 ## Set the building display
 ## building_name is the displayed name of the building
 func set_building(building : String, left : int):
 	# check if there is a texture
-	if not building in FILES:
-		print("Can't find a texture for the building")
+	if not building in BuildingsData.TEXTURES:
+		print("Can't find a texture for the building" + building)
 		return
 	
 	# Set the button display
-	$Building/BuildingSprite.texture = load(FILES.get(building))
+	$Building/BuildingSprite.texture = BuildingsData.TEXTURES.get(building)
 	$Building/Name.text = building
 	$Building/Left.text = "Restant : " + String(left)
 
