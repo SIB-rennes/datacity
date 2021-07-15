3d382fbe17bba987e71f647da202b71e6cf83d94
Mathis Robert <mathis.robert@sib.fr>
2021-07-15 17:00:21 +0200

    Added progress bar color

--- ui/bar.gd
+++ ui/bar.gd
@@ -8,3 +8,14 @@ func set_text(text: String):
 # Value from 0 to 100
 func set_percentage(percentage: int):
 	$VBoxContainer/TextureProgress.value = percentage
+	
+	## Color
+	var color
+	
+	# Red to Yellow
+	if percentage <= 50: 
+		color = Color.red.linear_interpolate(Color.yellow, percentage/50.0)
+	else: # Yellow to Green
+		color = Color.yellow.linear_interpolate(Color.green, (percentage-50)/50.0)
+		
+	$VBoxContainer/TextureProgress.tint_progress = color
