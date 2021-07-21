2838456a4a31edcd11c2b89a77ae7698acd4980d
Mathis Robert <mathis.robert@sib.fr>
2021-07-21 16:16:52 +0200

    Added an EventResult scene

---
+++ ui/EventRecompenses.gd
@@ -0,0 +1,55 @@
+extends MarginContainer
+
+
+signal close_results
+
+
+# Preload the Building buttons
+var building_button = preload("res://ui/building_button.tscn")
+
+# Building button container
+onready var building_container = $ScrollMargin/VBoxContainer/ScrollContainer/BuildingContainer
+
+
+
+func hide():
+	# Remove each building button
+	for child in building_container.get_children():
+		building_container.remove_child(child)
+	
+	.hide()
+
+
+
+func set_points_gained(points: int):
+	if points == 0:
+		return
+	
+	# Create the text
+	var text = "Datapoints "
+	
+	if points > 0:
+		text += "+"
+	else:
+		text += "-"
+	
+	text += String(points)
+	
+	# Set the text
+	$ScrollMargin/VBoxContainer/PointsGained.text = text
+
+
+
+## Adds a building button
+## building_name is the displayed name of the building
+func add_building(building_name: String):
+	var button = building_button.instance()
+	button.set_building(building_name)
+	
+	# Add as a child
+	building_container.add_child(button)
+
+
+
+func _on_QuitButton_pressed():
+	emit_signal("close_results")
