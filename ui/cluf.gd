035c164e1fdb473878f4668f90fe0683911eb813
Mathis Robert <mathis.robert@sib.fr>
2021-07-22 15:00:26 +0200

    Added a Credit window on the main menu

--- ui/cluf.gd
+++ ui/cluf.gd
@@ -4,3 +4,4 @@ extends MarginContainer
 func _on_Button_pressed():
 	# Removes the CLUF scene from the Tree
 	queue_free()
+
---
+++ ui/credits.gd
@@ -0,0 +1,7 @@
+extends MarginContainer
+
+
+
+
+func _on_Button_pressed():
+	hide()
