44f414ffdb450204df7e95c50ef355095784290e
Mathis Robert <mathis.robert@sib.fr>
2021-07-27 10:56:12 +0200

    Updated Credits and corrected url opening

--- ui/credits.gd
+++ ui/credits.gd
@@ -1,7 +1,27 @@
 extends MarginContainer
 
 
+onready var text_label = $MainContainer/ScrollMargin/VBoxContainer/MarginContainer/ScrollContainer2/RichTextLabel
+
+
+
+func _ready():
+	# If not a web export, connect urls to OS shell open
+	if OS.get_name() != "HTML5":
+		text_label.connect("meta_clicked", self, "_on_RichTextLabel_meta_clicked")
+		
+
+
+func _on_RichTextLabel_meta_clicked(meta):
+	var err = OS.shell_open(meta)
+	if err == OK:
+		print("Opened link '%s' successfully!" % meta)
+	else:
+		print("Failed opening the link '%s'!" % meta)
+
 
 
 func _on_Button_pressed():
 	hide()
+
+
