b2d64dc3d7b062f5a1afebb40e169ef0a11064e4
Mathis Robert <mathis.robert@sib.fr>
2021-07-27 09:56:27 +0200

    Integrated the new events

--- game_data/scenaristic_events.gd
+++ game_data/scenaristic_events.gd
@@ -20,27 +20,118 @@ const EVENTS_CONDITIONS = {
 		"built(x)": {"Maison 1": [1, 1], "Maison 2": [1, 1], "Maison 3": [1, 1]},
 		"not had_event(x)": ["PremieresMaisons"],
 	},
+	
 	### <=====> ###  Pedagogical events
 	"PublierDonneesTransports": {
 		"had_event(x)": ["PremieresMaisons"],
 		"not had_event(x)": ["PublierDonneesTransports"], # Not again 
 		"probability(x)": .3, # 30% chance after each building
 		"population_over(x)": 50 # The event won't trigger if the population count is under x
-	}
+	},
+	"DonneesUrbanisme": {
+		"had_event(x)": ["PremieresMaisons"],
+		"not had_event(x)": ["DonneesUrbanisme"], # Not again 
+		"probability(x)": .3, # 30% chance after each building
+		"population_over(x)": 50 # The event won't trigger if the population count is under x
+	},
+	"AutomatiserDonnees": {
+		"had_event(x)": ["PublierDonneesTransports", "DonneesUrbanisme"],
+		"not had_event(x)": ["AutomatiserDonnees"], # Not again 
+		"probability(x)": .3, # 30% chance after each building
+		"population_over(x)": 80 # The event won't trigger if the population count is under x
+	},
+	
+	
+	"InfosConference": {
+		"had_event(x)": ["PremieresMaisons"],
+		"not had_event(x)": ["InfosConference"], # Not again 
+		"probability(x)": .3, # 30% chance after each building
+		"population_over(x)": 50 # The event won't trigger if the population count is under x
+	},
+	"CitoyenEnerve": {
+		"had_event(x)": ["InfosConference"],
+		"not had_event(x)": ["CitoyenEnerve"], # Not again 
+		"probability(x)": .5, # 30% chance after each building
+		"population_over(x)": 120 # The event won't trigger if the population count is under x
+	},
+	
+	
+	"RetourConferencier_1": {
+		"had_event(x)":  ["PremieresMaisons"],
+		"not had_event(x)": ["RetourConferencier_1"], # Not again 
+		"probability(x)": .3, # 30% chance after each building
+		"population_over(x)": 50 # The event won't trigger if the population count is under x
+	},
+	"RetourConferencier_2": {
+		"had_event(x)":  ["RetourConferencier_1"], # Need the previous Event
+		"not had_event(x)": ["RetourConferencier_2"], # Not again 
+		"probability(x)": .3, # 30% chance after each building
+		"population_over(x)": 80 # The event won't trigger if the population count is under x
+	},
+	"RetourConferencier_3": {
+		"had_event(x)":  ["RetourConferencier_2"], # Need the previous Event
+		"not had_event(x)": ["RetourConferencier_3"], # Not again 
+		"probability(x)": .5, # 30% chance after each building
+		"population_over(x)": 120
+	},
+	
+	
+	"DonneesHopitaux": {
+		"had_event(x)":  ["PremieresMaisons"],
+		"not had_event(x)": ["DonneesHopitaux"], # Not again 
+		"probability(x)": .2, # 30% chance after each building
+		"population_over(x)": 80 # The event won't trigger if the population count is under x
+	},
+	
+	
+	"DroitsDonnees": {
+		"had_event(x)":  ["PremieresMaisons"],
+		"not had_event(x)": ["DroitsDonnees"], # Not again 
+		"probability(x)": .2, # 30% chance after each building
+		"population_over(x)": 80 # The event won't trigger if the population count is under x
+	},
 }
 
 
 const DIALOG_FILES = {
 	"MairieConstruite": "res://scenarios/beginning/mairie_built.json",
 	"PremieresMaisons": "res://scenarios/beginning/premieres_maisons.json",
+	
 	"PublierDonneesTransports": "res://scenarios/pedagogical/publier_donnees_transports.json",
+	"DonneesUrbanisme": "res://scenarios/pedagogical/donnees_urbanisme.json",
+	"AutomatiserDonnees": "res://scenarios/pedagogical/automatiser_publication_donnes.json",
+	
+	"InfosConference": "res://scenarios/beginning/infos_conference.json",
+	"CitoyenEnerve": "res://scenarios/beginning/citoyen_enerve.json",
+	
+	"RetourConferencier_1": "res://scenarios/pedagogical/retour_conferencier_1.json",
+	"RetourConferencier_2": "res://scenarios/pedagogical/retour_conferencier_2.json",
+	"RetourConferencier_3": "res://scenarios/pedagogical/retour_conferencier_3.json",
+	
+	"DonneesHopitaux": "res://scenarios/pedagogical/donnees_hopitaux.json",
+	
+	"DroitsDonnees": "res://scenarios/pedagogical/droits_donnees.json",
 }
 
 
 const SUMMARIES = {
-	"MairieConstruite": "La secrétaire de mairie veut vous voir.",
-	"PremieresMaisons": "La secrétaire de mairie veut vous voir.",
+	"MairieConstruite": "Votre adjointe veut vous voir.",
+	"PremieresMaisons": "Votre adjointe veut vous voir.",
+	
 	"PublierDonneesTransports": "Vous croisez un citoyen dans un parc.",
+	"DonneesUrbanisme": "Une citoyenne est venue vous voir.",
+	"AutomatiserDonnees": "Un citoyen est venu vous proposer un projet.",
+	
+	"InfosConference": "Vous croisez une citoyenne dans un parc.",
+	"CitoyenEnerve": "Un citoyen tient à vous parler.",
+	
+	"RetourConferencier_1": "Votre adjointe a un message pour vous.",
+	"RetourConferencier_2": "Le conférencier est revenu vous voir.",
+	"RetourConferencier_3": "Le conférencier est revenu vous voir.",
+	
+	"DonneesHopitaux": "Votre adjointe a une proposition à vous faire.",
+	
+	"DroitsDonnees": "Votre adjointe veut vous parler.",
 }
 
 
