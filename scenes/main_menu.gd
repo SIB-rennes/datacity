4b6de156e174165df9b74980faadfa9cdfab05d6
Mathis Robert <mathis.robert@sib.fr>
2021-07-29 10:58:10 +0200

    Added a Skip button to the top left of MainMenu

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -3,3 +3,7 @@ extends Control
 
 func on_fade_finished():
 	get_tree().change_scene("res://scenes/Introduction.tscn")
+
+
+func _on_SkipIntro_pressed():
+	get_tree().change_scene("res://scenes/CityBuilder.tscn")
